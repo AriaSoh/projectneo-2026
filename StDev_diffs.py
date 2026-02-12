8ce6ffcd5e43f1fb00735a0a7abf605e83d5b678
Akhila Anya Pisupati <akhilaanya@users.noreply.github.com>
2026-02-12 12:10:56 -0800

    Trying to plot histogram of cumulative probability differences

--- StDev_diffs.py
+++ StDev_diffs.py
@@ -1,5 +1,10 @@
 #Finding difference in impact probabilities for NASA and ESA asteroids
 from astropy import QTable
+import matplotlib.pyplot as pyplot
+import numpy as np
 
-nasa = QTable.read("NASA_data.csv")
-esa = QTable.read("ESA_data.csv")
+#Reading the files
+nasa = QTable.read("NASA_data.csv", format = 'csv')
+esa = QTable.read("ESA_data.csv", format = 'csv')
+
+
